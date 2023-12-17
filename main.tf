bf837d55fb18de446265f3d7ecaba0e32ae41fc0
Uzair Ahmed <uzair.pstc@gmail.com>
2023-12-17 06:25:12 +0000

    Created s3 bucket with terraform random naming

--- main.tf
+++ main.tf
@@ -4,7 +4,16 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.0"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
   }
+}
+
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -14,8 +23,16 @@ provider "random" {
 
 resource "random_string" "bucket_name" {
   length     = 16
-  special    = false
+  lower = true
+  upper = false
+  special = false
 
+}
+
+# Error 403 Access Denied faced when tried to create bucket with terraform apply.
+# Deleted terraform.tfstate file and again init, plan and apply terraform and issue got resolved, state file was using old bucket name
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
